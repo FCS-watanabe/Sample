f036a5864532af4b56193d7312527eed385a2063
watanosuke <syuu_watanabe@fcs.co.jp>
2016-11-04 10:25:18 +0000

    Create index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE HTML>
+
+<html>
+
+    <head>
+
+        <title>Example</title>
+
+    </head>
+
+    <body>
+
+
+
+        <h1>Hello World!</h1>
+
+        <?php
+
+            echo "Hi, I'm a PHP script!";
+
+        ?>
+
+
+
+     </body>
+
+</html>
